a8dd676457442b0949b89e623ce92b221c5e45c0
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-25 17:36:44 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIO 01 - ORIENTACAO A OBJ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace EXERCICIO_01___ORIENTACAO_A_OBJ{
+    class Program{
+        //-----> INICIO
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Retangulo r = new Retangulo();
+
+            Console.Write("Largura: ");
+                r.Largura = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+                r.Altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Area: " + r.Area().ToString("F2"));
+            Console.WriteLine("");
+            Console.Write("Perimetro: " + r.Area().ToString("F2"));
+            Console.WriteLine("");
+            Console.WriteLine("Diagonal" + r.Diagonal().ToString("F2"));
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //-----> FIM
+    }
+}
---
+++ EXERCICIO 01 - ORIENTACAO A OBJ/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCICIO_01___ORIENTACAO_A_OBJ{
+    //----------------------START | OBJ RETANGULO------------------------//
+    class Retangulo{//----> Atributos da classe
+        public double Largura;
+        public double Altura;
+    //------------------------ MET. CLASS -------------------------------//
+    public double Area(){ 
+        return Largura * Altura;
+    }
+    public double Perimeto(){
+        return 2 * (Largura * Altura);
+    }
+    public double Diagonal(){
+        return Math.Sqrt(Largura * Largura + Altura * Altura);    
+        }
+    }
+    //--------------------------- END --------------------------------//    
+}
